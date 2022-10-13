62fa6480180fa522495d036786d8cc3de8e3c30c
kamalu precious <kamalu.precious20@gmail.com>
2022-10-13 16:54:47 -0700

    edited

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
  * main - Function begins
+ * Description: 'and that piece of art is useful
  * Return: Function ends
  */
 
 int main(void)
 
 {
-
-	write('2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n', 59);
-
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 
 }
